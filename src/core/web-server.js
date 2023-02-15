04e782c1b342848158c47ccff661f9cf72117ece
Adrien <adriencompare@gmail.com>
2023-02-15 17:55:42 +0100

    Websocket avec socket io

--- src/core/web-server.js
+++ src/core/web-server.js
@@ -4,27 +4,26 @@ const userRoutes = require('../controllers/user.routes');
 const authRoutes = require('../controllers/auth.route');
 const cardRoutes = require('../controllers/card.route');
 const userCardRoutes = require('../controllers/user-card.routes');
-const tempGameRoutes = require('../controllers/temp-game.routes');
 
 const { sequelize } = require('../models/postgres.db')
-
-const {User} = require("../models/models/user.model");
-const {Card} = require("../models/models/card.model");
-const {User_card} = require("../models/models/user_card.model");
-const {Temp_Game} = require("../models/models/temp_game.model");
+const { User } = require("../models/models/user.model");
+const { Card } = require("../models/models/card.model");
+const { User_card } = require("../models/models/user_card.model");
+const http = require('http');
+const socketIo = require('socket.io');
 
 class WebServer {
   app = undefined;
   port = 3000;
   server = undefined;
+  io = undefined;
 
   constructor() {
     this.app = express();
-    require('dotenv').config()
+    require('dotenv').config();
 
     User.belongsToMany(Card, { through: User_card, foreignKey: 'id_user' });
     Card.belongsToMany(User, { through: User_card, foreignKey: 'id_card' });
-    User.hasOne(Temp_Game,{ through: Temp_Game, foreignKey: 'id_user' });
 
     sequelize.sync();
     // sequelize.sync({ force: true });
@@ -35,7 +34,10 @@ class WebServer {
   }
 
   start() {
-    this.server = this.app.listen(this.port, () => {
+    this.server = http.createServer(this.app);
+    this.io = socketIo(this.server);
+    this._initializeWebSocket();
+    this.server.listen(this.port, () => {
       console.log(`Example app listening on port ${this.port}`);
     });
     console.log(process.env.NODE_ENV);
@@ -50,7 +52,21 @@ class WebServer {
     this.app.use('/auth', authRoutes.initializeRoutes());
     this.app.use('/cards', cardRoutes.initializeRoutes());
     this.app.use('/user-cards', userCardRoutes.initializeRoutes());
-    this.app.use('/temp-games', tempGameRoutes.initializeRoutes());
+  }
+
+  _initializeWebSocket() {
+    this.io.on('connection', (socket) => {
+      console.log(`A user connected: ${socket.id}`);
+
+      socket.on('disconnect', () => {
+        console.log(`A user disconnected: ${socket.id}`);
+      });
+
+      socket.on('example-event', (data) => {
+        console.log(`Received example-event: ${data}`);
+        this.io.emit('example-event', data);
+      });
+    });
   }
 }
 
